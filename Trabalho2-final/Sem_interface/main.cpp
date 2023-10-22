397d6da22f76aa5a2dd3e91a930d2d7118385e13
Livia Ferrao <livia.ferrao16@hotmail.com>
2023-10-22 20:47:37 -0300

    tempo

--- Trabalho2-final/Sem_interface/main.cpp
+++ Trabalho2-final/Sem_interface/main.cpp
@@ -7,14 +7,10 @@
 #include "FIFO.h"
 #include "LRU.h"
 #include "OPT.h"
-#include <chrono> 
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    // Inicie a contagem do tempo
-    auto start_time = std::chrono::high_resolution_clock::now();
-
     // Leitura da entrada
     if (argc != 2) {
         cerr << "Uso: " << argv[0] << " <num_frames>" << endl;
@@ -32,17 +28,10 @@ int main(int argc, char *argv[]) {
         scanf("%d",&page);
         references.push_back(page);
     }
-
     // Construtor
     Simulador simulador(num_frames, references);
     // Iniciar a simulação
     simulador.start_simulation();
     // Imprimir resultados
     simulador.print_terminal();
-
-    // Pare a contagem do tempo
-    auto end_time = std::chrono::high_resolution_clock::now();
-    // Calcule a diferença de tempo
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
-    cout << "Tempo de execução: " << duration.count() << " milliseconds" << endl;
 }
